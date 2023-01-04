062f1dc1513624316d7f9a87a802d155b5329a5e
Kevin <challengerenlol@gmail.com>
2023-01-04 13:33:02 -0300

    Finished For Loop section. Almost finished While Loop section.

--- practica1.c
+++ practica1.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-   exerciseSeven();
+   exerciseThirteen();
 
    return 0;
 }
@@ -111,5 +111,107 @@ int exerciseSeven() {
     printf("El año %d no es bisiesto", year);
   }
   
+  return 0;
+}
+
+// Part 3: While Loop.
+// Ex 9: Write a program that show the integer numbers between 1 and 100.
+
+int exerciseNine() {
+  int number = 1;
+  while (number <= 100) {
+    printf("%d \n", number);
+    number += 1;
+  }
+
+  return 0;
+}
+
+// Ex 10: Write a program that shows the impair numbers between 1 and 100.
+
+int exerciseTen() {
+  int number = 1;
+
+  while (number <= 100) {
+    if (number % 2 != 0) {
+      printf("%d \n", number);
+    }
+    number += 1;
+  }
+  
+  return 0;
+}
+
+// Ex 11: Write a program that asks for two numbers and shows all the numbers that goes from the first to the second.
+// The input has to validate that the first number is less than the second.
+
+int exerciseEleven() {
+  int firstNumber, secondNumber;
+
+  printf("Nota. Se le va a pedir ingresar dos numeros, por favor asegurese de que el primer numero es mas chico que el segundo.\n");
+  printf("Por favor, ingrese el primer numero: ");
+  scanf("%d", &firstNumber);
+  printf("Por favor, ingrese el segundo numero: ");
+  scanf("%d", &secondNumber);
+
+  if (firstNumber >= secondNumber) {
+    printf("El segundo numero es mas chico que el primero. El programa se reiniciara.");
+    exerciseEleven();
+  }
+  while (firstNumber <= secondNumber) {
+    printf("%d \n", firstNumber);
+    firstNumber += 1;
+  }
+
+  return 0;
+}
+
+// Ex 12: Write a program that given a number, it determines if it's a prime number or not.
+
+int exerciseTwelve() {
+  int number, i = 1, counterOfDivisors = 0;
+
+  printf("Por favor, ingrese un numero: ");
+  scanf("%d", &number);
+
+  // If to filter negative numbers or the 0 (cases where the prime number is not defined).
+  if (number > 0) {
+    // While to go through all the numbers in between.
+    while(i <= number) {
+      // If to filter the 1 and the number itself in which cases the number is always divisible.
+      if (i != 1 && i != number && (number % i == 0)) {
+        counterOfDivisors += 1;
+      }
+
+      i += 1;
+    }
+    if (counterOfDivisors == 0) {
+    printf("El numero %d es primo.", number);
+    } else {
+      printf("El numero %d no es primo", number);
+    }
+  } else {
+    printf("\nEl numero ingresado no puede ser evaluado para determinar si es un numero primo.");
+  }
+
+  return 0;
+}
+
+// Ex 13: Write a program that asks for a number and shows the factorial of it.
+// Suggestion, define the factorial variable as a double.
+
+int exerciseThirteen() {
+  int number;
+  double factorial = 1.0;
+
+  printf("Por favor, ingrese un numero: ");
+  scanf("%d", &number);
+
+  while(number > 0) {
+    factorial = factorial * number;
+    number -= 1;
+  }
+  printf("Su factorial es: %.2f", factorial);
+
   return 0;
 }
