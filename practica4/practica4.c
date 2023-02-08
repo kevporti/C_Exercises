ba67b1f79c0276fb93881a60444f4c61ffc6d79a
Kev from Aure <challengerenlol@gmail.com>
2023-02-08 20:42:10 -0300

    Starting part 2.

--- practica4/practica4.c
+++ practica4/practica4.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
 int swap(int *x, int *y);
 int strlength(char *c);
 void strcopy(char *dest, char *src);
+void exerciseFive();
 
 int main() {
-  char string1[12] = "Hola mundo.", string2[12];
-  printf("%s, %s\n", string1, string2);
-  strcopy(string2, string1);
-  printf("%s, %s\n", string1, string2);
+  exerciseFive();
 
   return 0;
 }
@@ -47,4 +47,103 @@ void strcopy(char *s, char *t) {
     s[i] = t[i];
     i++;
   }
+}
+
+
+// Part 2: Structures.
+
+// Ex 3: Define a struct 'Punto' to represent a point in the plane, and an struct 'Rectangulo' that represents
+// a rectangle (A rectangle is represented by the upper left point and the lower right point).
+
+typedef struct {
+  int x;
+  int y;
+} Punto;
+
+typedef struct {
+  Punto *upperLeftPoint;
+  Punto *lowerRightPoint;
+} Rectangulo;
+
+
+// Ex 4: Define a function 'inRect' that determines whether a point is inside the rectangle or not.
+
+bool inRect(Punto point, Rectangulo rectangle) {
+  //Checking if the point is inside the rectangle from the upperLeftPoint.
+  if (point.x >= rectangle.upperLeftPoint->x && point.y <= rectangle.upperLeftPoint->y) {
+    //Checking if the point is inside the rectangle from the lowerRightPoint.
+    if (point.x <= rectangle.lowerRightPoint->x && point.y >= rectangle.lowerRightPoint->y) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
+
+void exerciseFour() {
+  Punto point1, point2, point3;
+  Rectangulo rectangle;
+
+  // Point to check.
+  point1.x = 2;
+  point1.y = 3;
+
+  // Points of rectangle.
+  // upperLeftPoint.
+  point2.x = 4;
+  point2.y = 7;
+  // lowerRightPoint.
+  point3.x = 6;
+  point3.y = 5;
+
+  //Rectangle.
+  rectangle.lowerRightPoint = &point3;
+  rectangle.upperLeftPoint = &point2;
+  if (inRect(point1, rectangle) == 1) {
+    printf("El punto (%d, %d) esta dentro del rectangulo.", point1.x, point1.y);
+  } else {
+    printf("El punto (%d, %d) NO esta dentro del rectangulo.", point1.x, point1.y);
+  }
+}
+
+
+// Ex 5: Define a struct 'Contacto' that has as fields: a string for the name of a person and a string
+// for the phone number.
+// Implement a function 'crearContacto' that asks the data and returns a 'Contacto' struct. Get memory
+// dinamically to store the data of the person.
+
+typedef struct {
+  char *name;
+  char *phone;
+} Contacto;
+
+Contacto* crearContacto() {
+  char temp1[100], temp2[100];
+  Contacto *createdContact = malloc(sizeof(Contacto));
+
+  // Asking for the name of the contact.
+  printf("Por favor, ingrese el nombre del contacto: ");
+  scanf("%s", temp1);
+  createdContact->name = malloc(sizeof(char) * (strlen(temp1) + 1));
+  strcpy(createdContact->name, temp1);
+
+  // Asing for the phone number of the contact.
+  printf("Por favor, ingrese el numero de telefono del contacto: ");
+  scanf("%s", temp2);
+  createdContact->phone = malloc(sizeof(char) * (strlen(temp2) + 1));
+  strcpy(createdContact->phone, temp2);
+
+  return createdContact;
+}
+
+void exerciseFive() {
+  Contacto *newContact = crearContacto();
+  printf("El nombre del contacto es: %s, y su numero es: %s", newContact->name, newContact->phone);
+
+  free(newContact->name);
+  free(newContact->phone);
+  free(newContact);
+
 }
